4db4545c36781c8c5bf96dba53e270729916502d
jjammar9 <jjam24306@gmail.com>
2026-05-22 08:29:21 +0200

    Initial Vue frontend foundation setup

---
+++ client/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-slate-100">
+    <h1 class="text-6xl font-bold text-blue-600">
+      GetYourTicket
+    </h1>
+  </div>
+</template>
